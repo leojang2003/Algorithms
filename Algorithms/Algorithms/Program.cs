a4355c725e8fbbdedc9af97ede0cbf48b6c4dd9a
leojang2003 <30883229+leojang2003@users.noreply.github.com>
2020-03-30 14:02:12 +0800

    Update Program.cs
    Test MoveEvenNodeToEndReverseOrder() function

--- Algorithms/Algorithms/Program.cs
+++ Algorithms/Algorithms/Program.cs
@@ -1,4 +1,4 @@
-﻿using Algorithms.LinkedList;
+using Algorithms.LinkedList;
 using Algorithms.Sort;
 using System;
 using System.Collections.Generic;
@@ -12,11 +12,48 @@ namespace Algorithms
     {
         static void Main(string[] args)
         {
-            int[] source = { 9, 8, 7, 6, 5, 4, 3, 2, 1 };
-            //SelectionSort.Sort2(source);
-            InsertionSort.Sort(source);
-            //Console.ReadKey();
+            //RemoveDuplicateFromSorted();
+            //SplitLinkedList();
+            //MoveHeadToAnotherList();
+            MoveEvenNodeToEndReverseOrder();
+            Console.ReadKey();            
+        }
 
+        public static void MoveEvenNodeToEndReverseOrder()
+        {
+            SingleLinkedList list = new SingleLinkedList();
+            //list.Insert(1);
+            list.Insert(2);
+            list.Insert(3);
+            list.Insert(4);
+            list.Insert(5);
+            list.Insert(6);
+            list.Insert(7);
+            list.MoveEvenNodeToEndReverseOrder();
+            list.Print(list.head);
+        }
+
+        public static void MoveHeadToAnotherList()
+        {
+            SingleLinkedList list = new SingleLinkedList();
+            list.Insert(1);
+            list.Insert(2);
+            list.Insert(3);
+
+            SingleLinkedList list2 = new SingleLinkedList();
+            list2.Insert(6);
+            list2.Insert(4);
+            list2.Insert(2);
+
+            var head2 = list.MoveFrontToAnotherList(list2.head);
+
+            list.Print(list.head);
+            Console.WriteLine();
+            list2.Print(head2);
+        }
+
+        public static void RemoveDuplicateFromSorted()
+        {
             SingleLinkedList list = new SingleLinkedList();
             list.Insert(1);
             list.Insert(2);
@@ -30,17 +67,29 @@ namespace Algorithms
             list.Insert(7);
             list.Insert(10);
 
-            //list.Print(list.head);
-
             list.RemoveDuplicateFromSorted();
             list.Print(list.head);
+        }
 
-            //var head2 = list.Split();
+        public static void SplitLinkedList()
+        {
+            SingleLinkedList list = new SingleLinkedList();
+            list.Insert(1);            
+            list.Insert(2);
+            list.Insert(3);
+            list.Insert(4);
+            list.Insert(5);
+            list.Insert(6);
+            list.Insert(7);
+            list.Insert(8);
+            list.Insert(9);
 
-            //list.Print(list.head);
-            //Console.WriteLine();
-            //list.Print(head2);
-            Console.Read();
+            list.Print(list.head);
+            var head2 = list.Split();
+
+            list.Print(list.head);
+            Console.WriteLine();
+            list.Print(head2);            
         }
     }
 }
