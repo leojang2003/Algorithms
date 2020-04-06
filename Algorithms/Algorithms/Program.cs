0ed0a058439293ab0b233ccd04f4e98e5b00a04b
leojang2003 <30883229+leojang2003@users.noreply.github.com>
2020-04-06 11:23:31 +0800

    Update Program.cs
    implement find K’th node from the end in a linked list

--- Algorithms/Algorithms/Program.cs
+++ Algorithms/Algorithms/Program.cs
@@ -22,8 +22,30 @@ namespace Algorithms
             //MergeTwoSortedList();
             //IntersectionOfTwoGivenSortedLinkedLists();
             //ReverseLinkedList();
-            ReverseEveryGroupOfKNodes();
+            //ReverseEveryGroupOfKNodes();
+            FindKthNodeFromTheEnd();
             Console.ReadKey();            
+        }
+
+        public static void FindKthNodeFromTheEnd()
+        {
+            SingleLinkedList list = new SingleLinkedList();
+            list.Insert(0);
+            list.Insert(1);
+            list.Insert(2);
+            list.Insert(3);
+            list.Insert(4);
+            list.Insert(5);
+            list.Insert(6);
+            list.Insert(7);
+            list.Insert(8);
+            list.Insert(9);
+            list.Insert(10);
+
+            list.Print(list.head);
+            Console.WriteLine();
+            Console.WriteLine(list.FindKthNodeFromTheEnd(6));            
+            Console.WriteLine();
         }
 
         public static void ReverseEveryGroupOfKNodes()
