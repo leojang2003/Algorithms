1d7b0d1784f9cd8baff05cd7af0885a842d01a8e
1051448tsb <1051448tsb@gmail.com>
2020-04-21 23:56:05 +0800

    Implement find length of longest common sequence

--- Algorithms/Algorithms/DynamicProgramming/LongestCommonSequence.cs
+++ Algorithms/Algorithms/DynamicProgramming/LongestCommonSequence.cs
@@ -8,20 +8,21 @@ namespace Algorithms.DynamicProgramming
 {
     public class LongestCommonSequence
     {
-        public string LCS(string[] input1, string[] input2)
+        public int LCS(string[] input1, string[] input2)
         {
             var m = input1.Length;
-            var n = input1.Length;
+            var n = input2.Length;
+
+            if (m == 0 || n == 0)
+                return "";
 
             if(input1[m-1] == input1[n-1])
             {
-                return LCS(input1.Take(m - 1).ToArray(), input2.Take(n - 1).ToArray()) + input1[m - 1];
+                return LCS(input1.Take(m - 1).ToArray(), input2.Take(n - 1).ToArray()) + 1;
             }
             else
             {
-                var sub1 = LCS(input1.Take(m - 1).ToArray(), input2);
-                var sub2 = LCS(input1.Take(m).ToArray(), input2.Take(n-1).ToArray());
-                return sub1.Length > sub2.Length ? sub1 : sub2;
+                return Math.Max(LCS(input1.Take(m - 1).ToArray(), input2), LCS(input1.Take(m).ToArray(), input2.Take(n - 1).ToArray()))
             }
         }
     }
