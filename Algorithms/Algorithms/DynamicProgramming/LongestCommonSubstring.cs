89ab424b123fc4528871add4d13f2db2703dfb56
1051448tsb <1051448tsb@gmail.com>
2020-04-27 23:10:19 +0800

    Add empty longest common substring function

---
+++ Algorithms/Algorithms/DynamicProgramming/LongestCommonSubstring.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.DynamicProgramming
+{
+    public class LongestCommonSubstring
+    {
+        public string Find()
+        {
+            return "";
+        }
+
+        // "ABDJEEFCF", "AJIJEFCFEJI"
+    }
+}
