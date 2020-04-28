02f35844b776be04cfb1f93cd302b03a0dfb1d9d
1051448tsb <1051448tsb@gmail.com>
2020-04-28 23:11:35 +0800

    Create lookup table for longest common substring

--- Algorithms/Algorithms/DynamicProgramming/LongestCommonSubstring.cs
+++ Algorithms/Algorithms/DynamicProgramming/LongestCommonSubstring.cs
@@ -12,7 +12,28 @@ namespace Algorithms.DynamicProgramming
         {
             return "";
         }
+                
+        public int[,] Lookup(string a, string b)
+        {
+            // default value of multidimensional array is 0
+            var lookup = new int[a.Length + 1, b.Length + 1];
 
-        // "ABDJEEFCF", "AJIJEFCFEJI"
+            for (int i = 1; i <= a.Length; i++)
+            {
+                for (int j = 1; j <= b.Length; j++)
+                {
+                    if (a.Substring(i - 1, 1) != b.Substring(j - 1, 1))
+                    {
+                        lookup[i, j] = 0;
+                    }
+                    else
+                    {
+                        lookup[i, j] = lookup[i - 1, j - 1] + 1;
+                    }
+                }
+            }
+
+            return lookup;
+        }
     }
 }
