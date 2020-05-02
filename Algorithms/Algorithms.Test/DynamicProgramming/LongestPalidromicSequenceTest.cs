ab5e33208bf12470075c3b6fafd7646699ea76ed
1051448tsb <1051448tsb@gmail.com>
2020-05-02 09:08:00 +0800

    Implement longest palidromic sequence

---
+++ Algorithms/Algorithms.Test/DynamicProgramming/LongestPalidromicSequenceTest.cs
@@ -0,0 +1,61 @@
+﻿using Algorithms.DynamicProgramming;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Test.DynamicProgramming
+{
+    public class LongestPalidromicSequenceTest
+    {
+        [Test]
+        public void Test1()
+        {
+            var sut = new LongestPalidromicSequence();
+            var test = "ABCDEDCBA";
+            var lcs = sut.Find(test, 0, test.Length - 1);
+            Assert.Contains("ABCDEDCBA", lcs);
+        }
+
+        [Test]
+        public void Test2()
+        {
+            var sut = new LongestPalidromicSequence();
+            var test = "ABCBADOOOPQRSTUVUTSRQ";
+            var lcs = sut.Find(test, 0, test.Length - 1);
+            Assert.Contains("QRSTUVUTSRQ", lcs);
+        }
+
+        [Test]
+        public void Test3()
+        {
+            var sut = new LongestPalidromicSequence();
+            var test = "ABCBAPQRQP";
+            var lcs = sut.Find(test, 0, test.Length - 1);
+            Assert.AreEqual(2, lcs.Count);
+            Assert.Contains("ABCBA", lcs);
+            Assert.Contains("PQRQP", lcs);
+        }
+
+        [Test]
+        public void Test4()
+        {
+            var sut = new LongestPalidromicSequence();
+            var test = "XXXX";
+            var lcs = sut.Find(test, 0, test.Length - 1);
+            Assert.AreEqual(1, lcs.Count);
+            Assert.Contains("XXXX", lcs);
+        }
+
+        [Test]
+        public void Test5()
+        {
+            var sut = new LongestPalidromicSequence();
+            var test = "QOAGBLCKTDSCPBJAZ";
+            var lcs = sut.Find(test, 0, test.Length - 1);            
+            Assert.Contains("ABCDCBA", lcs);
+        }
+    }
+}
---
+++ Algorithms/Algorithms/DynamicProgramming/LongestPalidromicSequence.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.DynamicProgramming
+{
+    public class LongestPalidromicSequence
+    {
+        // 0,4
+        // 1 3
+        // 2 2
+        // 
+
+        public List<string> Find(string input, int start, int end)
+        {
+            if (start <= end)
+            {
+                var x = input.Substring(start, 1);
+                var y = input.Substring(end, 1);
+
+                if (x == y)
+                {
+                    var result = new List<string>();
+                    var tmp = Find(input, start + 1, end - 1);
+                    // debug found error start 
+                    if (tmp.Count == 0)
+                    {
+                        if (start == end)
+                            result.Add(x);
+                        else
+                            result.Add(x + x);
+                    }
+                    // debug found error end                        
+                    else
+                    {
+                        foreach (var lcs in tmp)
+                        {
+                            result.Add(x + lcs + x);
+                        }
+                    }
+                    return result;
+                }
+                else
+                {
+                    var sub1 = Find(input, start + 1, end);
+                    var sub2 = Find(input, start, end - 1);
+                    if (sub1.Count() == 0 && sub2.Count > 0)
+                        return sub2;
+                    else if (sub2.Count() == 0 && sub1.Count > 0)
+                        return sub1;
+                    else if (sub1.Count() == 0 && sub2.Count == 0)
+                        return new List<string>();
+                    else
+                    {
+                        if (sub1.First().Length > sub2.First().Length)
+                            return sub1;
+                        else if (sub1.First().Length < sub2.First().Length)
+                            return sub2;
+                        else
+                            return sub1.Concat(sub2).ToList();
+                    }
+                }
+            }
+            return new List<string>();
+        }
+    }
+}
