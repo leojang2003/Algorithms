5ae2b883a3d56c1bc8f9d1742e0d2bc2286e6829
1051448tsb <1051448tsb@gmail.com>
2020-05-05 08:33:04 +0800

    Remove duplicate calculation by using Dictionary<string, int> to lookup

--- Algorithms/Algorithms.Test/DynamicProgramming/LongestRepeatSequenceTest.cs
+++ Algorithms/Algorithms.Test/DynamicProgramming/LongestRepeatSequenceTest.cs
@@ -46,5 +46,45 @@ namespace Algorithms.Test.DynamicProgramming
             var result = sut.Length(test, test.Length, test.Length);
             Assert.AreEqual(2, result);
         }
+
+        [Test]
+        public void Test5()
+        {
+            var sut = new LongestRepeatSequence();
+            var test = "ABAB";
+            var lookup = new Dictionary<string, int>();
+            var result = sut.LengthNoDupeCalc(test, test.Length, test.Length, lookup);
+            Assert.AreEqual(2, result);
+        }
+
+        [Test]
+        public void Test6()
+        {
+            var sut = new LongestRepeatSequence();
+            var test = "ATACTCGCA";
+            var lookup = new Dictionary<string, int>();
+            var result = sut.LengthNoDupeCalc(test, test.Length, test.Length, lookup);
+            Assert.AreEqual(4, result);
+        }
+
+        [Test]
+        public void Test7()
+        {
+            var sut = new LongestRepeatSequence();
+            var test = "ABCDACBD";
+            var lookup = new Dictionary<string, int>();
+            var result = sut.LengthNoDupeCalc(test, test.Length, test.Length, lookup);
+            Assert.AreEqual(3, result);
+        }
+
+        [Test]
+        public void Test8()
+        {
+            var sut = new LongestRepeatSequence();
+            var test = "CABQACQB";
+            var lookup = new Dictionary<string, int>();
+            var result = sut.LengthNoDupeCalc(test, test.Length, test.Length, lookup);
+            Assert.AreEqual(2, result);
+        }
     }
 }
--- Algorithms/Algorithms/DynamicProgramming/LongestRepeatSequence.cs
+++ Algorithms/Algorithms/DynamicProgramming/LongestRepeatSequence.cs
@@ -46,15 +46,40 @@ namespace Algorithms.DynamicProgramming
         public int Length(string x, int m, int n)
         {
             if (m == 0 || n == 0)
-            {   
+            {
                 return 0;
             }
-            
+
             if (x.Substring(m - 1, 1) == x.Substring(n - 1, 1) && m != n)
-            {   
+            {
                 return Length(x, m - 1, n - 1) + 1;
-            }            
+            }
             return Math.Max(Length(x, m - 1, n), Length(x, m, n - 1));
+        }
+
+        // Remove duplicate calculation by using memory
+        public int LengthNoDupeCalc(string x, int m, int n, Dictionary<string, int> lookup)
+        {
+            if (m == 0 || n == 0)
+            {
+                return 0;
+            }
+
+            string key = $"{m}|{n}"; // brilliant solution on web
+
+            if (!lookup.ContainsKey(key)) // should lookup in outer scope
+            {
+                if (x.Substring(m - 1, 1) == x.Substring(n - 1, 1) && m != n)
+                {
+                    return Length(x, m - 1, n - 1) + 1;
+                }
+                return Math.Max(Length(x, m - 1, n), Length(x, m, n - 1));
+            }
+
+            var value = -1;
+            lookup.TryGetValue(key, out value);
+            return value;
+                
         }
     }
 }
