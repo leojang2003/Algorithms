1e52bfe57a51fb57a0c1e58d8e18d2d7aabda0c9
leojang2003 <30883229+leojang2003@users.noreply.github.com>
2020-05-18 09:11:48 +0800

    Update LongestIncreasingSubsequence.cs
    Longest increasing subsequences

--- Algorithms/Algorithms/DynamicProgramming/LongestIncreasingSubsequence.cs
+++ Algorithms/Algorithms/DynamicProgramming/LongestIncreasingSubsequence.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,19 +8,80 @@ namespace Algorithms.DynamicProgramming
 {
     public class LongestIncreasingSubsequence
     {
-        public int LIS(int[] A, int i, int n, int previous)
+        int[] p; 
+
+        public int Length(int[] A)
         {
-            if (i == n)
-               return 0;
+            var lookup = Lookup(A);
+            var max = -1;
+            for(int i = 0; i < lookup.Length; i++)
+            {
+                if (lookup[i] > max)
+                    max = lookup[i];
+            }
 
-            int exc1 = LIS(A, i + 1, n, previous);
+            return max;
+        }
 
-            int incl = 0;
+        public List<int> PrintAny(int[] A)
+        {
+            var lookup = Lookup(A);
+            var list = new List<int>();
+            var max_index = -1;
+            var max_value = -1;
 
-            if (A[i] > previous)
-                incl = 1 + LIS(A, i + 1, n, previous);
+            for (int i = 0; i < lookup.Length; i++)
+            {
+                if (lookup[i] > max_value)
+                {
+                    max_index = i;
+                    max_value = lookup[i];
+                }
+                    
+            }
+            
+            while(lookup[max_index] >= 1)
+            {
+                list.Insert(0, A[max_index]);
 
-            return Math.Max(exc1, incl); 
+                if (lookup[max_index] == 1)
+                    break;
+
+                max_index = p[max_index];                
+            }
+
+            return list;
+        }
+
+        public int[] Lookup(int[] A)
+        {
+            var lookup = new int[A.Length];
+            p = new int[A.Length];
+
+            for (int j = 0; j < A.Length; j++)
+            {
+                lookup[j] = 1;
+                p[j] = j;
+            }                
+
+            for(int i = 0; i < A.Length; i++)
+            {
+                for(int j = 0; j < i; j++)
+                {
+                    if (A[i] > A[j])
+                    {
+                        // debug found error, should put this line before Math.Max
+                        if (lookup[j] + 1 > lookup[i])
+                            p[i] = j;
+
+                        lookup[i] = Math.Max(lookup[i], lookup[j] + 1);
+
+                        
+                    }                        
+                }
+            }
+
+            return lookup;
         }
     }
 }
