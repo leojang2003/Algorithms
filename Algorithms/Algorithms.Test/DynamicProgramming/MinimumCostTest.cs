e890d1ff1631b02bfdcba705f3dfc513233eb383
1051448tsb <1051448tsb@gmail.com>
2020-06-04 23:17:43 +0800

    Minimum cost

---
+++ Algorithms/Algorithms.Test/DynamicProgramming/MinimumCostTest.cs
@@ -0,0 +1,27 @@
+﻿using Algorithms.DynamicProgramming;
+using Algorithms.LinkedList;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Test.LinkedList
+{
+    public class MinimumCostTest
+    {
+        [Test]
+        public void Test1()
+        {
+            var sut = new MinimumCost();
+            var a = new int[5, 5];
+
+
+            var lcs = sut.Find("XXXXXX", "XXXXXX");
+            Assert.AreEqual(1, lcs.Count);
+            Assert.Contains("XXXXXX", lcs);
+        }
+        
+    }
+}
--- Algorithms/Algorithms/DynamicProgramming/MinimumCost.cs
+++ Algorithms/Algorithms/DynamicProgramming/MinimumCost.cs
@@ -7,20 +7,24 @@ using System.Threading.Tasks;
 namespace Algorithms.DynamicProgramming
 {
     public class MinimumCost
-    {      
+    {
         public int Lookup(int[,] input)
         {
             // use Array.GetLength(dimension);
             var d = new int[input.GetLength(0) + 1, input.GetLength(1) + 1];
-            
+
             for (int i = 1; i <= input.GetLength(1); i++)
             {
                 for (int j = 1; j <= input.GetLength(0); j++)
                 {
-                    if (d[i - 1, j] < d[i, j-1])
-                        d[i, j] = d[i-1,j] + input[i,j];
+                    if (i == 1)
+                        d[i, j] = d[i, j - 1] + input[i - 1, j - 1];
+                    else if (j == 1)
+                        d[i, j] = d[i - 1, j] + input[i - 1, j - 1];
+                    else if (d[i - 1, j] < d[i, j - 1])
+                        d[i, j] = d[i - 1, j] + input[i - 1, j - 1];
                     else if (d[i - 1, j] >= d[i, j - 1])
-                        d[i, j] = d[i, j - 1] + input[i, j];                    
+                        d[i, j] = d[i, j - 1] + input[i - 1, j - 1];
                 }
             }
 
